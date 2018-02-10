0b424caa37ebdc8dfa5310e96adb578b538211de
Phil S <philipstubbs13@gmail.com>
2018-02-10 15:48:36 -0600

    committing initial files. Attempting to start with the Letter.js file code first

---
+++ Letter.js
@@ -0,0 +1,32 @@
+// Letter.js: 
+// Contains a constructor, Letter. 
+// This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), 
+// depending on whether or not the user has guessed the letter. That means the constructor should define:
+
+// A string value to store the underlying character for the letter
+// A boolean value that stores whether that letter has been guessed yet
+// A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
+// A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+
+var Letter = function(character) {
+	// A string value to store the underlying character for the letter
+	this.character = character;
+	// A boolean value that stores whether that letter has been guessed yet
+	this.letterGuessed = false;
+	// A function that returns the underlying character if the letter has been guessed, 
+	//or a placeholder (like an underscore) if the letter has not been guessed
+	this.showCharacter = function() {
+		if (letterGuessed) {
+			return this.character;
+		}
+		else {
+			return "_";
+		}
+
+	}
+	this.checkValue = function() {
+
+	}
+}
+
+module.exports = Letter
