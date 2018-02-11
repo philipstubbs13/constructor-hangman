f8f1d1edad3f2148735d7c8a0373251ba29c4b2c
Phil S <philipstubbs13@gmail.com>
2018-02-10 23:19:06 -0600

    cleaning up code in Word constructor before moving on. Adding comments so that I don't forget what I just did.

--- Word.js
+++ Word.js
@@ -8,15 +8,15 @@
 // A function that takes a character as an argument and calls the guess function on each letter object 
 // (the second function defined in Letter.js)
 
-//Word depends on the Letter constructor. So, need to require Letter.
+//Word depends on the Letter constructor. So, we need to require Letter so that we can use it in the Word.js file (this file).
 var Letter = require("./Letter");
 
 var Word = function(myWord) {
 	//Take chosen word from word list.
 	this.myWord = myWord;
-	//This is an array of letters representing the letters of the underlying word.
+	//This is an array of letters representing the letters of the random chosen word.
 	this.letters = [];
-	//This is an array of underscores representing the number of underscores needed for a word 
+	//This is an array of underscores representing the number of underscores needed for the random chosen word 
 	//This is based on the number of letters in the word.
 	this.underscores = [];
 	//After we get a random word from the word list, I think I need to use the split method to add the letters to the this.letters array.
@@ -25,13 +25,13 @@ var Word = function(myWord) {
 		console.log(this.letters);
 		//Determine number of underscores needed based on length of this.letters array in the Word constructor.
 		numberUnderscoresNeeded = this.letters.length;
-		console.log("Underscores: " + numberUnderscoresNeeded);
+		//console.log("Underscores: " + numberUnderscoresNeeded);
 		//Create for loop that pushes the underscores to the this.underscores array in Word constructor.
 		for (var i=0; i < numberUnderscoresNeeded; i++ ) {
 			this.underscores.push("_ ");
 		}
-		console.log(this.underscores);
-		//Use the .join method to join each underscore needed for the word by a space.
+		//console.log(this.underscores);
+		//Use the .join method to join each underscore that we pushed to the this.underscores array by a space.
 		console.log(this.underscores.join(" "));
 	}
 }
@@ -40,4 +40,5 @@ var Word = function(myWord) {
 // var someWord = new Word ("Burnsville");
 // someWord.splitWord();
 
+//Export the Word constructor so that we can use/reference it in index.js.
 module.exports = Word;
