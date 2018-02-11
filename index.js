6b3aa797034ee01a009f976c2ec31255466d5564
Phil S <philipstubbs13@gmail.com>
2018-02-10 18:41:42 -0600

    adding start game functionality

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 var Word = require("./Word.js");
 var inquirer = require("inquirer");
+
+console.log("Welcome to the Hangman Game!")
+console.log("Theme: Minnesota cities")
+
+var readyToStartGame = [
+ {
+    type: 'confirm',
+    name: 'readyToPlay',
+    message: 'Are you ready to play?',
+    default: true
+  }
+];
+
+inquirer.prompt(readyToStartGame).then(answers => {
+	if (answers.readyToPlay){
+		console.log("Great! Let's begin...");
+		guessLetter();
+	}
+
+	else {
+		console.log("Good bye!");
+		return;
+	}
+});
+
+function guessLetter(){
+	inquirer.prompt([
+  {
+    name: "letter",
+    message: "Guess a letter"
+  }
+]).then(function(answers) {
+	console.log(answers.letter);
+});
+
+}
